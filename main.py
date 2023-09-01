8a6ddf49993e4251b7fe56aa6f6f464d54c5e093
aryansenpaii <aryansenpai256@gmail.com>
2023-09-02 04:25:47 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
 import webbrowser
 
-cap = cv2.VideoCapture(1)
-detector = cv2.QRCodeDetector() 
+cap = cv2.VideoCapture(1) #Here the value 1 represent the camera number and it depends on system to decide the camera number 
+detector = cv2.QRCodeDetector() #assigning alias to various open-cv functionalities
 
 while True:
-    _, img = cap.read()
+    _, img = cap.read() 
     data,_,__ = detector.detectAndDecode(img)
     if (data):
         a=data
@@ -14,7 +14,7 @@ while True:
     if cv2.waitKey(1) == ord("q"):
         break
 
-b=webbrowser.open(str(a))
-cap.release()
-cv2.destroyAllWindows()
+b=webbrowser.open(str(a)) #Opens the browser using the qr_code data recieved
+cap.release() #freeing the camera
+cv2.destroyAllWindows()#closes the camera view window
 
